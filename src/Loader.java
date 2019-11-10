7528ca2728320b90fda2f90cebf39419b868c7e0
AlexRzh <a.rzhevtsev@netrika.ru>
2019-11-10 21:30:56 +0300

    добавила себе лет жизни

--- src/Loader.java
+++ src/Loader.java
@@ -2,17 +2,23 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 public class Loader
 {
     public static void main(String[] args) {
+
         Calendar calendar = Calendar.getInstance();
 
         calendar.set(1989, Calendar.FEBRUARY, 2);
 
-        DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy - EEE");
+        Calendar calendar2 = Calendar.getInstance();
 
-        for(int i = 0; i < 30; i++)
+        int currentYearCount = calendar2.get(Calendar.YEAR) - calendar.get(Calendar.YEAR);
+
+        DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy - EEEE", new Locale("ru"));
+
+        for(int i = 0; i <= currentYearCount; i++)
         {
             Date date = calendar.getTime();
             calendar.add(Calendar.YEAR, 1);
